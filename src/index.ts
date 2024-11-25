f88e6830963449780083d6ec1c7857fa545ca86d
Alireza <ialirezamhi@gmail.com>
2024-11-25 21:24:21 +0330

    starter update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { hooks, Plugin } from 'webito-plugin-sdk';
+import axios from 'axios';
+import webito from 'webito-plugin-sdk'
 
-const myPlugin = new Plugin('MyAwesomePlugin');
+const starter = new webito.WebitoPlugin('starter');
 
-myPlugin.registerHook(hooks, async (userData) => {
+starter.registerHook(webito.hooks.messagesCreate, ({ target }) => 
+    {
+    // send otp
+    axios.post('https://example.com/sendotp', { mobile: target.mobile })
 });
 
-myPlugin.registerHook(hooks, async (productData) => {
+
+starter.registerHook(webito.hooks.productsCreate, () => {
+
+    // send message on products create
+    starter.executeHook(webito.hooks.messagesCreate, {})
 });
